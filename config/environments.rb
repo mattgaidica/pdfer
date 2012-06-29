0dfd8ddcae887050c740e90a9c4f8280e398c240
mattgaidica <matt@syllabuster.com>
2012-06-29 03:15:14 +0000

    production DB

--- config/environments.rb
+++ config/environments.rb
@@ -1,4 +1,5 @@
 RACK_ENV = ENV['RACK_ENV'] || 'development' unless defined? RACK_ENV
 
 config = YAML.load(ERB.new(File.read('./config/database.yml')).result)
+ActiveRecord::Base.logger = Logger.new(STDOUT)
 ActiveRecord::Base.establish_connection(config[settings.environment.to_s])
