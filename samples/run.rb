9d524b25cdf8b35f3b2325bb7a09f1679fdb4341
Matt Gaidica <matt@syllabuster.com>
2012-06-30 15:48:08 +0000

    full tree structure built

--- samples/run.rb
+++ samples/run.rb
@@ -1,15 +1,21 @@
-
 require "docsplit"
 
-puts "converting to pdf..."
-Docsplit.extract_pdf('syllabuster.html')
+jobs_path = "/var/www/pdfer/samples"
+file = "syllabuster.html"
+pdf = [file.split(".").first, ".pdf"].join
+
+puts "converting to pdf..."    
+Docsplit.extract_pdf(file)
 
 puts "making images..."
 system "mkdir images"
-Docsplit.extract_images("syllabuster.pdf", :size => '400x', :format => [:png])
+Docsplit.extract_images(pdf, :size => '400x', :format => [:png])
 system "mkdir images/small && mv *.png images/small"
-Docsplit.extract_images("syllabuster.pdf", :size => '1200x', :format => [:png])
+Docsplit.extract_images(pdf, :size => '1200x', :format => [:png])
 system "mkdir images/large && mv *.png images/large"
 
 puts "extracting text..."
-Docsplit.extract_text(Dir["syllabuster.pdf"], :ocr => true, :output => 'sample.txt')
+Docsplit.extract_text(Dir[pdf], :ocr => true, :output => 'text')
+
+puts "moving pdf into folder..."
+system "mkdir pdf && mv #{pdf} pdf/#{pdf}"
