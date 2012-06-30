40ab2f70e0d065ee62ae2533c1e0c626ffa11113
Matt Gaidica <matt@syllabuster.com>
2012-06-30 15:10:57 +0000

    html convert

--- samples/run.rb
+++ samples/run.rb
@@ -1,9 +1,12 @@
 
 require "docsplit"
 
+Docsplit.extract_pdf('sample.html')
+=begin
 system "mkdir images"
 Docsplit.extract_images("sample.pdf", :size => '400x', :format => [:png])
 system "mkdir images/small && mv *.png images/small"
 Docsplit.extract_images("sample.pdf", :size => '1200x', :format => [:png])
 system "mkdir images/large && mv *.png images/large"
-Docsplit.extract_text(Dir["sample.pdf"], :ocr => true, :output => 'storage/text') 
+Docsplit.extract_text(Dir["sample.pdf"], :ocr => true, :output => 'sample.txt') 
+=end
