e88e82523a0883c63774f4a52d466aff7638496d
Matt Gaidica <matt@syllabuster.com>
2012-06-30 15:29:45 +0000

    sylly website full convert

--- samples/run.rb
+++ samples/run.rb
@@ -1,12 +1,15 @@
 
 require "docsplit"
 
-Docsplit.extract_pdf('sample.html')
-=begin
+puts "converting to pdf..."
+Docsplit.extract_pdf('syllabuster.html')
+
+puts "making images..."
 system "mkdir images"
-Docsplit.extract_images("sample.pdf", :size => '400x', :format => [:png])
+Docsplit.extract_images("syllabuster.pdf", :size => '400x', :format => [:png])
 system "mkdir images/small && mv *.png images/small"
-Docsplit.extract_images("sample.pdf", :size => '1200x', :format => [:png])
+Docsplit.extract_images("syllabuster.pdf", :size => '1200x', :format => [:png])
 system "mkdir images/large && mv *.png images/large"
-Docsplit.extract_text(Dir["sample.pdf"], :ocr => true, :output => 'sample.txt') 
-=end
+
+puts "extracting text..."
+Docsplit.extract_text(Dir["syllabuster.pdf"], :ocr => true, :output => 'sample.txt')
