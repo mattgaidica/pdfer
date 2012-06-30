9f7f7ce994a64f8f3d7057a312077a7c0a99f341
Matt Gaidica <matt@syllabuster.com>
2012-06-30 14:39:30 +0000

    ocr working

--- samples/run.rb
+++ samples/run.rb
@@ -1,3 +1,9 @@
 
 require "docsplit"
-Docsplit.extract_images("sample.pdf", :size => '100x', :format => [:png])
+
+system "mkdir images"
+Docsplit.extract_images("sample.pdf", :size => '400x', :format => [:png])
+system "mkdir images/small && mv *.png images/small"
+Docsplit.extract_images("sample.pdf", :size => '1200x', :format => [:png])
+system "mkdir images/large && mv *.png images/large"
+Docsplit.extract_text(Dir["sample.pdf"], :ocr => true, :output => 'storage/text') 
